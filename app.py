072ebb00a1fbe3eb9a0b672a25c5960b0371d946
EmHall <em.cm.hall@gmail.com>
2020-09-18 13:42:27 +0000

    add registration

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -18,10 +19,15 @@ mongo = PyMongo(app)
 
 
 @app.route("/")
-@app.route("/get_tasks")
-def get_tasks():
-    tasks = mongo.db.categories.find()
-    return render_template("help.html", tasks=tasks)
+@app.route("/get_help")
+def get_help():
+    help = mongo.db.categories.find()
+    return render_template("help.html", help=help)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
 
 
 if __name__ == "__main__":
